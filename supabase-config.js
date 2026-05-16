a5bed9b5c691a5cce1044228ffc95e61ca8e51d7
diegossami-tech <diegossami@gmail.com>
2026-05-16 19:12:17 -0300

    Fix Supabase project URL

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.BACKNOTES_SUPABASE = {
-  url: 'https://azyqdtqxjjihaszkorqm.supabase.co',
+  url: 'https://azyqdtqxijihaszkorqm.supabase.co',
   anonKey: 'sb_publishable_sXSa7IWtR9wVd6lYYdSkCw_ZZI0Rvqz',
 };
